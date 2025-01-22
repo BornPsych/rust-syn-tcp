d3b1282e327f3b7415e42299f8f0148316400bca
Yogesh Shahi <yogeshshahi120@gmail.com>
2025-01-23 04:29:07 +0530

    feat: added async runtime to tcp listener

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use clap::{Parser, Subcommand};
-use std::ops::RangeInclusive;
 use std::process;
+use std::{ops::RangeInclusive, time::Duration};
+use stream::{client, server};
+use tokio;
 
 mod connect;
 mod serve;
-
+mod stream;
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 #[command(propagate_version = true)]
@@ -52,35 +54,29 @@ fn port_in_range(s: &str) -> Result<u16, String> {
 
 fn main() {
     let cli = Cli::parse();
+    let runtime = tokio::runtime::Runtime::new().unwrap();
 
     match &cli.command {
         Commands::Connect { host, port } => {
             println!("connect to {}:{}", host, port);
-
-            let run_result = connect::run(host, port);
-            match run_result {
-                Ok(()) => {
-                    process::exit(0);
+            runtime.block_on(async {
+                tokio::select! {
+                    _ = client() => {}
+                    _ = tokio::signal::ctrl_c() => {}
                 }
-                Err(e) => {
-                    println!("failed - {}", e);
-                    process::exit(1);
-                }
-            }
+            });
         }
+
         Commands::Serve { bind_host, port } => {
             println!("bind to {}:{}", bind_host, port);
-
-            let run_result = serve::run(bind_host, port);
-            match run_result {
-                Ok(()) => {
-                    process::exit(0);
+            runtime.block_on(async {
+                tokio::select! {
+                    _ = server() => {}
+                    _ = tokio::signal::ctrl_c() => {}
                 }
-                Err(e) => {
-                    println!("failed - {}", e);
-                    process::exit(1);
-                }
-            }
+            });
         }
     }
+
+    runtime.shutdown_timeout(Duration::from_secs(0));
 }
